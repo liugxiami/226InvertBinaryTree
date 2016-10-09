ce4cd9c57293dde576ace24ef269e6c1c97c9340
liugxiami <xiamiliu@gmail.com>
2016-10-09 16:09:38 -0500

    Update Main.java

--- src/com/ccsi/Main.java
+++ src/com/ccsi/Main.java
@@ -28,17 +28,18 @@ public class Main {
         return root;
     }
     //2.loop
-    public static TreeNode revertTree2(TreeNode root){
-        if(root==null) return null;
-        Stack<TreeNode> stack=new Stack<>();
-        stack.push(root);
-        while(!stack.isEmpty()){
-            TreeNode curr=stack.pop();
+    public static TreeNode invertTree1(TreeNode root){
+        if(root==null)return root;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode curr=queue.poll();
+            if(curr==null)return null;
             TreeNode temp=curr.left;
             curr.left=curr.right;
             curr.right=temp;
-            if(curr.left!=null) stack.push(curr.left);
-            if(curr.right!=null) stack.push(curr.right);
+            if(curr.left!=null)queue.offer(curr.left);
+            if(curr.right!=null)queue.offer(curr.right);
         }
         return root;
     }
